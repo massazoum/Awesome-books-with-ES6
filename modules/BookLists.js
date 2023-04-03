464353533a92f36feec52edf47f67926d719547d
massa <zoumaniguimassa12@gmail.com>
2023-04-03 18:10:32 +0000

    Refactor all functions to arrow functions

--- modules/BookLists.js
+++ modules/BookLists.js
@@ -4,13 +4,13 @@ export class Book {
     this.author = author;
   }
  }
- 
-export class BookList {
+
+ export class BookList {
   static bookListElement = document.getElementById('book-list');
   static books = JSON.parse(localStorage.getItem('books')) || [];
   static addBookForm = document.getElementById('add-book-form');
 
-  static handleAddBook(event) {
+  static handleAddBook = (event) => {
     event.preventDefault();
     const titleInput = document.getElementById('title-input');
     const authorInput = document.getElementById('author-input');
@@ -22,26 +22,26 @@ export class BookList {
     authorInput.value = '';
   }
 
-  static handleRemoveBook(event) {
+  static handleRemoveBook = (event) => {
     if (event.target.classList.contains('remove-button')) {
       const index = parseInt(event.target.dataset.index);
       BookList.removeBook(index);
     }
   }
 
-  static addBook(book) {
+  static addBook = (book) => {
     BookList.books.push(book);
     localStorage.setItem('books', JSON.stringify(BookList.books));
     BookList.displayBooks();
   }
 
-  static removeBook(index) {
+  static removeBook = (index) => {
     BookList.books.splice(index, 1);
     localStorage.setItem('books', JSON.stringify(BookList.books));
     BookList.displayBooks();
   }
 
-  static displayBooks() {
+  static displayBooks = () => {
     BookList.bookListElement.innerHTML = '';
     BookList.books.forEach((book, index) => {
       const div = document.createElement('div');
@@ -51,7 +51,7 @@ export class BookList {
     });
   }
 
-  static init() {
+  static init = () => {
     BookList.addBookForm.addEventListener('submit', BookList.handleAddBook);
     BookList.bookListElement.addEventListener('click', BookList.handleRemoveBook);
     BookList.displayBooks();
@@ -61,55 +61,3 @@ export class BookList {
 BookList.init();
 
 
-// export class BookList {
-//   constructor() {
-//     this.bookListElement = document.getElementById('book-list');
-//     this.books = JSON.parse(localStorage.getItem('books')) || [];
-//     this.addBookForm = document.getElementById('add-book-form');
-
-//     this.addBookForm.addEventListener('submit', this.handleAddBook.bind(this));
-//     this.bookListElement.addEventListener('click', this.handleRemoveBook.bind(this));
-//     this.displayBooks();
-//   }
-
-//   static handleAddBook(event) {
-//     event.preventDefault();
-//     const titleInput = document.getElementById('title-input');
-//     const authorInput = document.getElementById('author-input');
-//     const title = titleInput.value;
-//     const author = authorInput.value;
-//     const book = new Book(title, author);
-//     this.addBook(book);
-//     titleInput.value = '';
-//     authorInput.value = '';
-//   }
-
-//   static handleRemoveBook(event) {
-//     if (event.target.classList.contains('remove-button')) {
-//       const index = parseInt(event.target.dataset.index);
-//       this.removeBook(index);
-//     }
-//   }
-
-//   static addBook(book) {
-//     this.books.push(book);
-//     localStorage.setItem('books', JSON.stringify(this.books));
-//     this.displayBooks();
-//   }
-
-//   static removeBook(index) {
-//     this.books.splice(index, 1);
-//     localStorage.setItem('books', JSON.stringify(this.books));
-//     this.displayBooks();
-//   }
-
-//   static displayBooks() {
-//     this.bookListElement.innerHTML = '';
-//     this.books.forEach((book, index) => {
-//       const div = document.createElement('div');
-      
-//       div.innerHTML = `<div class="jshtml"> <div class="jshtml1"><h2> ${book.title} </h2><p> ${book.author}</p></div> <button data-index="${index}" class="remove-button">Remove</button></div>`;
-//       this.bookListElement.appendChild(div);
-//     });
-//   }
-// }
--- modules/initializeNavigation.js
+++ modules/initializeNavigation.js
@@ -7,26 +7,27 @@ const navL2 = document.querySelector('.L2');
 const navL3 = document.querySelector('.L3');
 
 
-export function F1() {
+export const F1 = () => {
  sectioncontact.style.display = 'none';
  sectionbook.style.display = 'block';
  showdiv.style.display = 'none';
  console.log('koqekrq');
 }
 
-export function F2() {
+export const F2 = () => {
  showdiv.style.display = 'block';
  sectioncontact.style.display = 'none';
  sectionbook.style.display = 'none';
  console.log('koqekrq');
 }
 
-export function F3() {
+export const F3 = () => {
  sectioncontact.style.display = 'block';
  sectionbook.style.display = 'none';
  showdiv.style.display = 'none';
  console.log('koqekrq');
 }
+
 
 navL1.addEventListener('click', F1);
 navL2.addEventListener('click', F2);
