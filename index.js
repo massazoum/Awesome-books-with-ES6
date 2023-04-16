f2884f587bd170cd7f75105bcf3a3d694337d9de
massa <zoumaniguimassa12@gmail.com>
2023-04-16 17:09:55 +0000

    Update

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ import { F1, F2, F3 } from './modules/initializeNavigation.js';
 import { Book, BookList } from './modules/BookLists.js';
 
 const bookList = new BookList();
+
+
+navL1.addEventListener('click', F1);
+navL2.addEventListener('click', F2);
+navL3.addEventListener('click', F3);
